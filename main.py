2d8988eec18aad3573467b70359904e6ad99789b
Eric Piraux <eric.piraux@stib.brussels>
2020-10-26 17:26:14 +0100

    move turtle using common function

--- main.py
+++ main.py
@@ -1,33 +1,41 @@
 import turtle
 
 pos = (0, 0)
+dot = turtle.Turtle(shape="circle", visible=True)
+dot.shapesize(0.5, 0.5)
+dot.color("red")
+dot.penup()
+
+def move(dir, delegate):
+    directions = {
+        "Right": 0,
+        "Up": 90,
+        "Left": 180,
+        "Down": 270
+                  }
+    turtle.onkeypress(None, dir)  # Désactive la touche Left
+    #print(directions[dir])
+    t.setheading(directions[dir])
+    t.fd(10)
+    dot.setpos(t.pos())
+    turtle.onkeypress(delegate, dir)  # Réassocie la touche Left à la fonction
+
 
 def move_right():
-    turtle.onkeypress(None, "Right")  # Désactive la touche Left
-    t.setheading(0)
-    t.fd(10)
-    turtle.onkeypress(move_right, "Right")  # Réassocie la touche Left à la fonction
+    move("Right", move_right)
 
 def move_up():
-    turtle.onkeypress(None, "Up")  # Désactive la touche Left
-    t.setheading(90)
-    t.fd(10)
-    turtle.onkeypress(move_up, "Up")  # Réassocie la touche Left à la fonction
+    move("Up", move_up)
 
 def move_left():
-    turtle.onkeypress(None, "Left")  # Désactive la touche Left
-    t.setheading(180)
-    t.fd(10)
-    turtle.onkeypress(move_left, "Left")  # Réassocie la touche Left à la fonction
+    move("Left", move_left)
 
 def move_down():
-    turtle.onkeypress(None, "Down")  # Désactive la touche Left
-    t.setheading(270)
-    t.fd(10)
-    turtle.onkeypress(move_down, "Down")  # Réassocie la touche Left à la fonction
+    move("Down", move_down)
 
 screen = turtle.Screen()
 t = turtle.Turtle()
+
 
 #t.dot(10, "blue")
 
